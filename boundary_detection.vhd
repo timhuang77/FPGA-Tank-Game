34ebc1d612c9cf1621815c032f7c7158dabc0a16
Tim Huang <tim81517lh@gmail.com>
2018-11-29 18:05:53 -0600

    commit before thursday evening

--- boundary_detection.vhd
+++ boundary_detection.vhd
@@ -6,8 +6,12 @@ use work.tank_functions.all;
 
 entity boundary_detection is
 	port(
-		clk, rst : in std_logic;
-		
+		clk : in std_logic;
+		off_screen_flag : out std_logic;
+		obj_width : in integer;
+		obj_height : in integer;
+		obj_pos_x : in integer;
+		obj_pos_y : in integer
 	);
 end entity boundary_detection;
 
@@ -15,6 +19,16 @@ architecture behavioral of boundary_detection is
 
 	
 begin
-	
+	process(clk) begin
+		if (obj_pos_x + obj_width/2 < 639 and 
+			obj_pos_x - obj_width/2 > 0 and
+			obj_pos_y + obj_height/2 < 479 and
+			obj_pos_y - obj_height/2 > 0) then
+			off_screen_flag <= '0';
+		else 
+			off_screen_flag <= '1'; 
+		end if;
+		
+	end process;
 	
 architecture behavioral;
